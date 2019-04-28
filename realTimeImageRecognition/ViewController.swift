533e694d32e62002fae895488502af852b311129
gan gan <mynameisgan10@gmail.com>
2019-04-28 20:38:17 +0800

    avfoundation camera setup

--- realTimeImageRecognition/ViewController.swift
+++ realTimeImageRecognition/ViewController.swift
@@ -7,12 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func prepareCamera() {
+        let captureSession = AVCaptureSession()
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(captureDeviceInput)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
     }
 
 
