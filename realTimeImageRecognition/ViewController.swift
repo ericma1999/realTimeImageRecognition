1f748f1f45d33fba21c471f06b042ab1de3038cf
gan gan <mynameisgan10@gmail.com>
2019-04-28 20:32:46 +0800

    Initial Commit

---
+++ realTimeImageRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  realTimeImageRecognition
+//
+//  Created by Gan on 28/4/19.
+//  Copyright © 2019 Gan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
